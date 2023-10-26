40a157013b7e1d66b240a4860db4a57171e9b943
Henry Wilde <henrydavidwilde@gmail.com>
2023-10-26 20:27:45 +0100

    Implement table querist and extractor

--- src/centhesus/api.py
+++ src/centhesus/api.py
@@ -3,6 +3,7 @@
 import json
 import warnings
 
+import pandas as pd
 import requests
 
 from centhesus.constants import API_ROOT
@@ -72,9 +73,6 @@ class CensusAPI:
         """
         Make a call to, and retrieve some data from, the API.
 
-        The only validation we do is to check the status code of the
-        response.
-
         Parameters
         ----------
         url : str
@@ -83,8 +81,8 @@ class CensusAPI:
         Returns
         -------
         data : dict or None
-            Data from the response of this API call if it is successful,
-            and `None` otherwise.
+            JSON data from the response of this API call if it is
+            successful, and `None` otherwise.
         """
 
         self._current_url = url
@@ -95,8 +93,27 @@ class CensusAPI:
 
         return self._current_data
 
-    def query_table(self, population_type, *dimensions, area_type="nat"):
-        """Retrieve the JSON for a table from the API."""
+    def _query_table_json(self, population_type, area_type, dimensions):
+        """
+        Retrieve the JSON for a table query from the API.
+
+        Parameters
+        ----------
+        population_type : str
+            Population type to query. See `centhesus.POPULATION_TYPES`.
+        area_type : str
+            Area type to query.
+            See `centhesus.AREA_TYPES_BY_POPULATION_TYPE`.
+        dimensions : list of str
+            Dimensions to query.
+            See `centhesus.DIMENSIONS_BY_POPULATION_TYPE`.
+
+        Returns
+        -------
+        data : dict or None
+            JSON data from the API call if it is successful, and `None`
+            otherwise.
+        """
 
         base = "/".join((API_ROOT, population_type, "census-observations"))
         parameters = f"area-type={area_type}&dimensions={','.join(dimensions)}"
@@ -105,3 +122,76 @@ class CensusAPI:
         data = self.get(url)
 
         return data
+
+    def query_table(self, population_type, area_type, dimensions, use_id=True):
+        """
+        Query and convert a JSON response to a Pandas data frame.
+
+        Parameters
+        ----------
+        population_type : str
+            Population type to query. See `centhesus.POPULATION_TYPES`.
+        area_type : str
+            Area type to query.
+            See `centhesus.AREA_TYPES_BY_POPULATION_TYPE`.
+        dimensions : list of str
+            Dimensions to query.
+            See `centhesus.DIMENSIONS_BY_POPULATION_TYPE`.
+        use_id : bool, default True
+            If `True` (the default) use the ID for each dimension and
+            area type. Otherwise, use the full label.
+
+        Returns
+        -------
+        data : pandas.DataFrame or None
+            Data frame containing the data from the API call if it is
+            successful, and `None` otherwise.
+        """
+
+        data_dict = self._query_table_json(
+            population_type, area_type, dimensions
+        )
+
+        if data_dict is None or "observations" not in data_dict:
+            return None
+
+        records = _extract_records_from_observations(
+            data_dict["observations"], use_id
+        )
+
+        columns = (area_type, *dimensions, "count")
+        data = pd.DataFrame(records, columns=columns)
+        data["population_type"] = population_type
+
+        return data
+
+
+def _extract_records_from_observations(observations, use_id):
+    """
+    Extract record information from a set of JSON observations.
+
+    Parameters
+    ----------
+    observations : dict
+        Dictionary of dimension options and count for the observation.
+    use_id : bool
+        If `True`, use the ID for each dimension option and area type.
+        Otherwise, use the full label.
+
+    Returns
+    -------
+    records : list of tuple
+        List of records to be formed into a data frame.
+    """
+
+    option = f"option{'_id' * use_id}"
+
+    records = []
+    for observation in observations:
+        record = (
+            *(dimension[option] for dimension in observation["dimensions"]),
+            observation["observation"],
+        )
+        records.append(record)
+
+    return records
