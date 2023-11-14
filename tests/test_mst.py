325e1e33707c467635cfb44637dda16d7bdfc5a0
Henry Wilde <henrydavidwilde@gmail.com>
2023-11-14 13:33:38 +0000

    Separate instantiation tests

--- tests/test_mst.py
+++ tests/test_mst.py
@@ -20,20 +20,13 @@ def st_api_parameters(draw):
 
     population_type = draw(st.sampled_from(POPULATION_TYPES))
     area_type = draw(
-        st.sampled_from(AREA_TYPES_BY_POPULATION_TYPE[population_type])
+        st.sampled_from(AREA_TYPES_BY_POPULATION_TYPE[population_type]),
     )
     dimensions = draw(
-        st.one_of(
-            (
-                st.just(None),
-                st.sets(
-                    st.sampled_from(
-                        DIMENSIONS_BY_POPULATION_TYPE[population_type]
-                    ),
-                    min_size=1,
-                ),
-            )
-        )
+        st.sets(
+            st.sampled_from(DIMENSIONS_BY_POPULATION_TYPE[population_type]),
+            min_size=1,
+        ).map(sorted),
     )
 
     return population_type, area_type, dimensions
@@ -52,11 +45,49 @@ def test_init(params):
     assert isinstance(mst, MST)
     assert mst.population_type == population_type
     assert mst.area_type == area_type
+    assert mst.dimensions == dimensions
 
-    if dimensions is None:
-        assert mst.dimensions == DIMENSIONS_BY_POPULATION_TYPE[population_type]
-    else:
-        assert mst.dimensions == dimensions
+    assert isinstance(mst.api, CensusAPI)
+    assert mst.domain == "domain"
+
+    get_domain.assert_called_once_with()
+
+
+@given(st_api_parameters())
+def test_init_none_area_type(params):
+    """Test instantiation of the MST class when area type is None."""
+
+    population_type, _, dimensions = params
+
+    with mock.patch("centhesus.mst.MST.get_domain") as get_domain:
+        get_domain.return_value = "domain"
+        mst = MST(population_type, None, dimensions)
+
+    assert isinstance(mst, MST)
+    assert mst.population_type == population_type
+    assert mst.area_type is None
+    assert mst.dimensions == dimensions
+
+    assert isinstance(mst.api, CensusAPI)
+    assert mst.domain == "domain"
+
+    get_domain.assert_called_once_with()
+
+
+@given(st_api_parameters())
+def test_init_none_dimensions(params):
+    """Test instantiation of the MST class when dimensions is None."""
+
+    population_type, area_type, _ = params
+
+    with mock.patch("centhesus.mst.MST.get_domain") as get_domain:
+        get_domain.return_value = "domain"
+        mst = MST(population_type, area_type, None)
+
+    assert isinstance(mst, MST)
+    assert mst.population_type == population_type
+    assert mst.area_type == area_type
+    assert mst.dimensions == DIMENSIONS_BY_POPULATION_TYPE[population_type]
 
     assert isinstance(mst.api, CensusAPI)
     assert mst.domain == "domain"
