58f9717bb9fca4da5273f6e198ee64c960b66e32
Henry Wilde <henrydavidwilde@gmail.com>
2023-11-14 13:42:04 +0000

    Write base test for domain feature getter

--- tests/test_mst.py
+++ tests/test_mst.py
@@ -2,6 +2,7 @@
 
 from unittest import mock
 
+import pandas as pd
 from census21api import CensusAPI
 from census21api.constants import (
     AREA_TYPES_BY_POPULATION_TYPE,
@@ -30,6 +31,22 @@ def st_api_parameters(draw):
     )
 
     return population_type, area_type, dimensions
+
+
+@st.composite
+def st_feature_metadata_parameters(draw):
+    """Create a parameter set and feature metadata for a test."""
+
+    population_type, area_type, dimensions = draw(st_api_parameters())
+
+    feature = draw(st.sampled_from(("area-types", "dimensions")))
+    items = [area_type] if feature == "area-types" else dimensions
+    metadata = pd.DataFrame(
+        ((item, draw(st.integers())) for item in items),
+        columns=("id", "total_count"),
+    )
+
+    return population_type, area_type, dimensions, feature, metadata
 
 
 @given(st_api_parameters())
@@ -92,4 +109,28 @@ def test_init_none_dimensions(params):
     assert isinstance(mst.api, CensusAPI)
     assert mst.domain == "domain"
 
+    get_domain.assert_called_once_with()
+
+
+@given(st_feature_metadata_parameters())
+def test_get_domain_of_feature(params):
+    """Test the domain of a feature can be retrieved correctly."""
+
+    population_type, area_type, dimensions, feature, metadata = params
+
+    with mock.patch("centhesus.mst.MST.get_domain") as get_domain:
+        get_domain.return_value = "domain"
+        mst = MST(population_type, area_type, dimensions)
+
+    with mock.patch("centhesus.mst.CensusAPI.query_feature") as query:
+        query.return_value = metadata
+        domain = mst._get_domain_of_feature(feature)
+
+    assert isinstance(domain, dict)
+
+    items = [area_type] if feature == "area-types" else dimensions
+    assert list(domain.keys()) == items
+    assert list(domain.values()) == metadata["total_count"].to_list()
+
+    query.assert_called_once_with(population_type, feature, *items)
     get_domain.assert_called_once_with()
